035c51bd267b3c81ca543a6c276f1b163e3a542f
Aashray <aashray446@gmail.com>
2022-06-09 12:52:12 +0530

    <AngularUpdate> : [Updated Angular to V14]

--- VartulWeb/README.md
+++ VartulWeb/README.md
@@ -1,6 +1,6 @@
 # VartulWeb
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.1.
 
 ## Development server
 
--- VartulWeb/src/app/app.component.spec.ts
+++ VartulWeb/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'VartulWeb'`, () => {
+  it(`should have as title 'vartulWeb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('VartulWeb');
+    expect(app.title).toEqual('vartulWeb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('VartulWeb app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('vartulWeb app is running!');
   });
 });
